e787872a2546854d542a0ad244417f957ba44321
Charithdewmina <charithdewmina2580@gmail.com>
2026-07-19 00:45:13 +0530

    Implement main menu

--- main.c
+++ main.c
@@ -3,6 +3,22 @@
 
 int main()
 {
-
+int option;
+    printf("==================================================\n");
+    printf("              MOVIE SEAT MANAGER                  \n");
+    printf("==================================================\n");
+    printf("\n\n     Welcome to the Movie Ticket System        \n\n\n");
+    printf("--------------------------------------------------\n\n");
+    printf(" 1. View Movies & Showtimes\n");
+    printf(" 2. View Seat Map\n");
+    printf(" 3. Book Tickets\n");
+    printf(" 4. Cancel Booking\n");
+    printf(" 5. Search Booking\n");
+    printf(" 6. Revenue Report\n");
+    printf(" 7. Exit\n\n");
+    printf("--------------------------------------------------\n\n");
+    printf("Select an option (1-7) : ");
+    scanf("%d",&option);
+    printf("\n");
     return 0;
 }
